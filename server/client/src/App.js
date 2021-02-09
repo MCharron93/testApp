8048e17c487ceec365129da2ab6dff858cdaff19
MCharron93 <michelle.charron93@gmail.com>
2021-02-09 16:44:39 -0700

    Refactoring bootstrap pull and import

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
@@ -7,6 +8,16 @@ function App() {
        <h1>Hello World</h1>
        <p>Here is a p tag that I just used with the emmet abbreviation snippet</p>
       </header>
+      <body>
+        <div className="row">
+          <div className="col-6">
+            <p>Here is another section in the web page</p>
+          </div>
+          <div className="col-6">
+            <p>Here is another test for bootstrap</p>
+          </div>
+        </div>
+      </body>
     </div>
   );
 }
