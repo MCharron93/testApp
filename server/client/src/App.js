4fa76e3d751d3bc9beea1944abf1c809cf3a1adf
MCharron93 <michelle.charron93@gmail.com>
2021-02-09 16:34:04 -0700

    Emmet changes and testing

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
        <h1>Hello World</h1>
+       <p>Here is a p tag that I just used with the emmet abbreviation snippet</p>
       </header>
     </div>
   );
